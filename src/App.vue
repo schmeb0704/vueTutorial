94d97fcfdb1a98729f08ceaa8c2e15fe0e9b84e0
schmeb0704 <seanauditor0704@gmail.com>
2024-11-02 12:38:56 +0800

    refactor: convert to composition api syntax

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+
+<script>
+import {ref} from 'vue'
+export default{
+  setup(){
+    const name = 'john doe'
+    const status = ref("active")
+    const tasks = ref([
+      'task 1',
+      'task 2', 
+      'task 3'
+    ])
+
+    const toggleStatus = () => {
+      
+      if(status.value === "active"){
+        status.value = 'pending'
+      } else if(status.value === 'pending'){
+        status.value = 'inactive'
+      }else{
+        status.value = 'active'
+      }
+    }
+
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    }
+  }
+}
+</script>
+<template>
+<h1>{{ name }}</h1>
+<p v-if="status === 'active'">User is active</p>
+<p v-else-if="status === 'pending'">User is pending</p>
+<p v-else>User is inactive</p>
+
+<h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+</h3>
+<br>
+<button @click="toggleStatus">Change status</button>
+</template>
+
+<style>
+h1{
+  color: red
+}
+</style>
