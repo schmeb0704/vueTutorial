803e0fd7fe90b6ef69a5c196b2d466190fa79024
schmeb0704 <seanauditor0704@gmail.com>
2024-11-02 17:23:11 +0800

    refactor: convert hrefs to RouterLinks

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import jobData from '@/jobs.json'
 import { ref , defineProps} from 'vue'
 import JobListing from './JobListing.vue';
+import { RouterLink } from 'vue-router';
 
 const jobs = ref(jobData)
 defineProps({
@@ -16,7 +17,7 @@ defineProps({
 <template>
   <section class="bg-blue-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
-      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center" id="browse-jobs-header">
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -28,10 +29,10 @@ defineProps({
     </div>
   </section>
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-      <a
-        href="/jobs"
+      <RouterLink
+        to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
+        >View All Jobs</RouterLink
       >
     </section>
 </template>
