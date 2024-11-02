aee68cec93c8fb613cd5b1cd2439d65e33731e9b
schmeb0704 <seanauditor0704@gmail.com>
2024-11-02 17:11:43 +0800

    refactor: change anchor tags to RouterLink

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,20 +18,20 @@ import logo from '@/assets/img/logo.png'
             </a>
             <div class="md:ml-auto">
               <div class="flex space-x-2">
-                <a
-                  href="index.html"
+                <RouterLink
+                  to="/"
                   class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
+                  >Home</RouterLink
                 >
-                <a
-                  href="jobs.html"
+                <RouterLink
+                  to="/jobs"
                   class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
+                  >Jobs</RouterLink
                 >
-                <a
-                  href="add-job.html"
+                <RouterLink
+                  to="/jobs/add"
                   class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
+                  >Add Job</RouterLink
                 >
               </div>
             </div>
