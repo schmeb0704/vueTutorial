7ac323e2776c1fa5cbde66c912a0fe5e46155137
schmeb0704 <seanauditor0704@gmail.com>
2024-11-02 12:44:56 +0800

    chore: format code

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
-
 <script setup>
-import {ref} from 'vue'
-    const name = 'john doe'
-    const status = ref("active")
-    const tasks = ref([
-      'task 1',
-      'task 2', 
-      'task 3'
-    ])
+import { ref } from 'vue'
+const name = 'john doe'
+const status = ref('active')
+const tasks = ref(['task 1', 'task 2', 'task 3'])
 
-    const toggleStatus = () => {
-      
-      if(status.value === "active"){
-        status.value = 'pending'
-      } else if(status.value === 'pending'){
-        status.value = 'inactive'
-      }else{
-        status.value = 'active'
-      }
-    }
-
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending'
+  } else if (status.value === 'pending') {
+    status.value = 'inactive'
+  } else {
+    status.value = 'active'
+  }
+}
 </script>
-<template>
-<h1>{{ name }}</h1>
-<p v-if="status === 'active'">User is active</p>
-<p v-else-if="status === 'pending'">User is pending</p>
-<p v-else>User is inactive</p>
 
-<h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-</h3>
-<br>
-<button @click="toggleStatus">Change status</button>
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </h3>
+  <br />
+  <button @click="toggleStatus">Change status</button>
 </template>
 
 <style>
-h1{
-  color: red
+h1 {
+  color: red;
 }
 </style>
