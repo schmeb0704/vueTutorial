b7cb201e3e7fc7c8eb86b4a503c225bbbbe194f1
schmeb0704 <seanauditor0704@gmail.com>
2024-11-02 14:34:38 +0800

    feat: create job listings component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
 import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
   <Navbar></Navbar>
   <Hero></Hero>
   <HomeCards></HomeCards>
+  <JobListings></JobListings>
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  Job listings
+</template>
