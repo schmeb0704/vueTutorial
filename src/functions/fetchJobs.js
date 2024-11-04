4eb142e5144b2358611d9660db3dc49894abfb69
Sean Ben <seanauditor0704@gmail.com>
2024-11-05 02:49:06 +0800

    refactor: use env variables for reusability

--- src/functions/fetchJobs.js
+++ src/functions/fetchJobs.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000'
+// const BASE_URL = '/api/'
+const BASE_URL = import.meta.env.VITE_BASE_URL
+console.log(BASE_URL)
 
 const fetchJobData = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/jobs`)
     const data = response.data
+    // console.log(data)
 
     return data
   } catch (error) {
